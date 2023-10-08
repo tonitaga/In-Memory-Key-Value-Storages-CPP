3dee7a7c2efc606cd07ac7d438a2fcf5708ed383
tonitaga <gubaydullin.nurislam@gmail.com>
2023-10-09 01:01:04 +0300

    try something with hash_map

--- src/in_memory_key_value/unordered_map/unordered_map.h
+++ src/in_memory_key_value/unordered_map/unordered_map.h
@@ -1,12 +1,26 @@
 #ifndef TRANSACTIONS_LIBRARY_CPP_UNORDERED_MAP_H
 #define TRANSACTIONS_LIBRARY_CPP_UNORDERED_MAP_H
 
+#include <vector>
+#include <iostream>
+#include <forward_list>
 
 // ttl Transactions Template Library
 namespace ttl {
-    template <typename Key, typename Value>
+    template <typename Key, typename Value, typename Hash = std::hash<Key>>
     class unordered_map {
     public:
+        using value_type = std::pair<const Key, Value>;
+        using key_type   = Key;
+
+    private:
+        struct node {
+            value_type data;
+            size_t cache;
+        };
+
+        std::forward_list<node> elements_;
+        std::vector<typename std::forward_list<node>::iterator> pointers_;
     };
 }
 
--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,10 @@
-#include "bplus_tree.h"
 #include "unordered_map.h"
-#include "red_black_tree.h"
+
+#include <iostream>
+#include <limits>
+#include <unordered_map>
 
 int main() {
-    ttl::unordered_map<int, int> unordered_map;
-    ttl::red_black_tree<int, int> rb_tree;
-    ttl::bplus_tree<int, int> bp_tree;
-
+    std::forward_list<int> list;
     return 0;
 }
